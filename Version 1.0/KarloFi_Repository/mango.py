6f0dc2c988a5f0b26aec6db315bb13d57c3e085c
nerdyperson16 <67539794+nerdyperson16@users.noreply.github.com>
2022-05-13 21:00:23 -0700

    Version 1.0 - First Version
    Very unstable, in a sense. But, the very first update. I don't recommend using this version as there are ads in the videos sometimes and autoplay doesn't work correctly. But isn't that always the first program?

---
+++ Version 1.0/KarloFi_Repository/mango.py
@@ -0,0 +1,43 @@
+print("K-Tech Application Development Company")
+print("Kar-LoFi Main Program - Python")
+print("Version 1.0")
+
+print("Importing Statements")
+from gpiozero import MotionSensor
+from subprocess import call
+import RPi.GPIO as GPIO
+import time
+from time import sleep
+from sys import exit
+import webbrowser
+import os
+
+print("Detecting Hardware Alterations")
+pir = MotionSensor(4)
+GPIO.setmode(GPIO.BCM)
+#GPIO.setup(8, GPIO.OUT)
+
+print("Integrating Code")
+websiteopen = "false"
+multicheck = "false"
+
+def my_function(multicheck):
+    if websiteopen == "true" and multicheck == "false":
+        webbrowser.open("https://www.youtube.com/watch?v=5qap5aO4i9A")
+
+while True:
+	if pir.motion_detected:
+		print("Motion Detected!")
+		websiteopen = "true"
+		#GPIO.output(8, GPIO.HIGH)
+		#time.sleep(0.5)
+		multicheck = my_function(multicheck)
+		mutlicheck = "true"
+		sleep(20)
+	else:
+		print("No Motion")
+		websiteopen = "false"
+		if websiteopen == "false" : os.system("killall chromium-browser")
+		multicheck = "false"
+		#GPIO.output(8, GPIO.LOW)
+		time.sleep(1) 
